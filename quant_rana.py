f6ba08957052987679fd134d350d8a6ab281ff1c
Rana Böğrekci <41337505+reyna07@users.noreply.github.com>
2024-10-31 13:41:25 -0400

    Add files via upload

--- quant_rana.py
+++ quant_rana.py
@@ -12,15 +12,15 @@ import torchvision
 pytorch_model = torch.load("cnn_model.pth")
 device = torch.device("cpu")
 #device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-target = "DPUCV2DX8G_v70"
-quant_mode = 'test'
+target = "DPUCV2DX8G_ISA1_C20B104"
+quant_mode = 'calib'
 batch_size = 32
 input = torch.randn([batch_size, 1, 14, 612])
 
 
 quantizer = torch_quantizer(
     quant_mode, pytorch_model, (input), device=device, target=target)
-
+print("quantization is done!")
     # Get the converted model to be quantized.
 quant_model = quantizer.quant_model
 
